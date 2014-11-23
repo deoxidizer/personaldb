f6f448c18aafa0856b98a2dcaac8955c2818fd78
Baris <Baris@Senkal97.ac.at>
2014-11-23 17:20:44 +0100

    BarisVersion

---
+++ Baris_Version/Flug_INSY_Projekt_1.Semester/Flug_INSY_Projekt_1.Semester/DetailedView.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Flug_INSY_Projekt_1.Semester
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1(PersonalundBerechtigung selectedPersonalundBerechtigung)
+        {
+            InitializeComponent();
+            //einfügen von Code zum Anzeigen der Einträge hier
+        }
+
+
+        private void UPDATE_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+
+    }
+}
